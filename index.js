eeb37e1ea43c356c83b9362140aa10636f8dfed0
Nshuti Jabes <nshutij7@gmail.com>
2022-08-25 19:46:07 +0200

    initialised connection to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB");
+    }catch(error){
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoose disconnected!")
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoose connected!")
+})
 
 app.listen("8800", () => {
-    console.log("Connected ")
+    connect()
+    console.log("Connected to backend")
 })
