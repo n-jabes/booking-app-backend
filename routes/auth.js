f6e649de1c6277db5fb1fb6d4ec3badac5480ecb
Nshuti Jabes <nshutij7@gmail.com>
2022-08-29 12:28:10 +0200

    created the hotel model

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,12 @@ import express from "express";
 const router = express.Router()
 
 router.get("/", (req,res) => {
-    res.send("Hello, this is auth")
+    res.send("Hello, this is auth endpoint")
 })
+
+router.get("/register", (req,res) => {
+    res.send("Hello, this is auth register endpoint")
+})
+
 
 export default router;
