a9a4046d63f0a814891db20cad98059f4d32299d
Nshuti Jabes <nshutij7@gmail.com>
2022-09-03 11:15:37 +0200

    added register api

--- models/User.js
+++ models/User.js
@@ -19,6 +19,6 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-})
+}, {timestamps: true})
 
 export default mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { register, login } from "../controllers/authentication.js";
 
 const router = express.Router();
 
-router.get("/register", (req,res) => {
-    res.send("Hello, this is auth register endpoint")
-})
+router.post("/register", register)
+router.post("/login", login)
 
 
 export default router;
