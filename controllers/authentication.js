fabc7aa5e146be0a48478ca3f9c9d319d23806bf
Nshuti Jabes <nshutij7@gmail.com>
2022-09-03 19:07:12 +0200

    created user login function with jwt authentication and bcryptjs password encryption

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { createError } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 export const register = async (req,res,next) => {
     try {
@@ -24,11 +25,18 @@ export const login = async (req,res,next) => {
         const user = await User.findOne({username: req.body.username})
         if(!user) return next(createError(404, "User not found!"))
 
-        const isPasswordCorrect = bcrypt.compare(req.body.password, user.password)
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if(!isPasswordCorrect) return next(createError(400, "Wrong username or password!"))
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT)
         
-        const { password, isAdmin, ...otherDetails} = user._doc
-        res.status(200).json({...otherDetails})
+        const {password, isAdmin, ...otherDetails} = user._doc
+        res
+            .cookie("access_token", token, {
+                httpOnly: true
+            })
+            .status(200)
+            .json({...otherDetails})
     } catch (error) {
         next(error)
     }
